fa892a674b9be862833b538496ce1e7093a49e89
Evan Hedgecock <evanjameshedgecock@gmail.com>
2024-12-29 19:27:12 -0700

    Create menu

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+
+def main():
+
+    
+
+    print('Welcome to your loan manager!\n\nWhat would you like to do?')
+    print()
+    selection = -1
+
+    while(selection < 1 or selection > 5):
+        try:
+            printMenu()
+            print()
+            selection = int(input('Enter menu number: '))
+            print()
+        except:
+            print()
+            printMenu()
+            print()
+            print('Only enter number.')
+            try:
+                selection = int(input('Enter menu number: '))
+            except:
+                continue
+
+def printMenu():
+     print('1) View Loans\n' +
+           '2) Add Loans\n' +
+           '3) Edit Loans\n' +
+           '4) Simulate Loan Payments\n' +
+           '5) Quit')
+
+main()
+
+
+
