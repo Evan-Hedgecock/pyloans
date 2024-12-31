15a22093376455f2f3373aa8f03296180913f133
Evan Hedgecock <evanjameshedgecock@gmail.com>
2024-12-31 00:48:39 -0700

    Can add loans, will add to loans.db

--- loan.py
+++ loan.py
@@ -1,13 +1,62 @@
-import sqlite3
+import datetime
 
-# Load loans from ./loans.db
-def loadLoans():
-    con = sqlite3.connect('loans.db')
-    cur = con.cursor()
-    f = open('loans.db')
-    try:
-        if f.read() == '':
-            print('Creating loans.db')
-            cur.execute('CREATE TABLE loans(id, name, balance, interest)')
-    except UnicodeDecodeError:
-       print('loans.db already created') 
+class Loan:
+ 
+    def __init__(self, name, amount, percent, interest, expense):
+        self.name = name
+        self.principal = amount
+
+        # Interest percent
+        self.percent = percent 
+
+        # Accrued interest
+        if interest == None:
+            self.interest = 0
+        else:
+            self.interest = interest 
+
+        # Interest expense
+        if expense == None:
+            self.expense = 0
+        else:
+            self.expense = expense 
+        
+        # Used to track amount of days to accrue interest
+        self.lastUpdate = datetime.date.today()
+        
+    def __str__(self):
+        total = self.getBalance()
+        return f'{self.name}: {total} @ {self.percent}%'
+
+    def makePayment(self, payment):
+        # Make payment to principal first
+        self.principal -= payment
+        # Leftover amount goes to interest
+        if self.principal < 0:
+            self.interest += self.principal
+            self.principal == 0
+
+    # Interest is accrued daily (365 days)
+    # Interest is accrued only based on principal
+    def accrueInterest(self, days=None):
+        today = datetime.date.today()
+        if today == self.lastUpdate:
+            return 
+
+        # Will accrue days since last update unless days are provided
+        if days == None:
+            accrueDays = (today - self.lastUpdate).days
+        else:
+            accrueDays = days
+
+        for i in range(accrueDays):
+            dailyInterest = (self.principal * self.percent) / 365
+            self.interest += dailyInterest
+            self.expense += dailyInterest
+   
+    def getBalance(self):
+        return self.principal + self.interest
+
+    def dataArray(self):
+        return [self.name, self.principal, self.percent,
+                self.interest, self.expense, str(self.lastUpdate)]
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import loan
-import sys
+from loan import Loan
+import sqlite3
 
 def main():
 
-    loan.loadLoans()
+    loadLoans()
 
     print('Welcome to your loan manager!\n\nWhat would you like to do?')
     print()
@@ -25,6 +25,9 @@ def main():
             except:
                 continue
 
+    if selection == 2:
+        addLoan()
+
 
 def printMenu():
      print('1) View Loans\n' +
@@ -33,7 +36,96 @@ def printMenu():
            '4) Simulate Loan Payments\n' +
            '5) Quit')
 
+def addLoan():
+    name = None
+    amount = None
+    percent = None
+    interest = None
+    expense = None
+    
+    while name == None:
+        name = input('Enter loan name: ')
+        if name == '':
+            name = None
+            print('\nCannot leave blank')
+
+    while amount == None:
+        try:
+            amount = float(input('Enter loan amount: '))
+        except:
+            amount = None
+            print('\nInvalid entry, only valid decimal point values allowed')
+
+    while percent == None:
+        try:
+            percent = float(input('Enter interest rate: '))
+        except:
+            percent = None
+            print('\nInvalid entry, only valid decimal point values allowed')
+
+    selection = input('Does this loan have accrued interest? [y/N](Default=N) ')
+    if selection.lower() == 'y':
+        while interest == None:
+            try:
+                interest = float(input('Enter accrued interest: '))
+            except:
+                interest = None
+                print('\nInvalid entry, only valid decimal point values allowed')
+    else:
+        print('Setting accrued interest to 0')
+        print('You can edit this loan to change later\n')
+
+    selection = input('Would you like to include any interest' + 
+                      ' you have already paid? [y/N](Default=N) ')
+    if selection.lower() == 'y':
+        while expense == None:
+            try:
+                expense = float(input('Enter past paid interest: '))
+            except:
+                expense = None
+                print('\nInvalid entery, only valid decimal point values allowed')
+    else:
+        print('Setting interest expense to 0')
+        print('You can add any past paid interest to this loan later\n')
+
+    if interest != None:
+        amount -= interest
+
+    loan = Loan(name, amount, percent, interest, expense)
+    print(f'Created new loan:\n{loan}')
+    saveLoan(loan)
+
+
+# Load loans from ./loans.db
+def loadLoans():
+    con = sqlite3.connect('loans.db')
+    cur = con.cursor()
+    # interest = accrued interest
+    # expense = interest expense
+    # percent = interest APR
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS loans(
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            principal REAL NOT NULL,
+            interest REAL,
+            expense REAL,
+            percent REAL NOT NULL,
+            lastUpdate TEXT
+           )
+    ''')    
+
+    for row in cur.execute('SELECT * FROM loans'):
+        print(row)
+
+def saveLoan(loan):
+    con = sqlite3.connect('loans.db')
+    cur = con.cursor()
+
+    cur.execute('INSERT INTO loans(name, principal, interest, expense, percent, lastUpdate)' +
+                ' VALUES(?, ?, ?, ?, ?, ?)', loan.dataArray())
+
+    con.commit()
+
+
 main()
-
-
-
